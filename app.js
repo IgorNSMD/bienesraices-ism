e4752e3184b31e1ddb493205588d3a068ef20cf9
IgorNSMD <insanmartind@gmail.com>
2023-04-28 00:23:14 -0400

    P19

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
+var csrf = require('csurf');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -36,6 +37,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Habilitar CSRF
+app.use( csrf({cookie: true}) )
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express - Bienes Raices-ISM - MySQL - P18' });
+  res.render('index', { title: 'Express - Bienes Raices-ISM - MySQL - P19' });
 });
 
 module.exports = router;
