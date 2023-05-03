ca392ba770f8492af90676ffd266c22c445cfd27
IgorNSMD <insanmartind@gmail.com>
2023-05-03 16:43:59 -0400

    P38

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express - Bienes Raices-ISM - MySQL - P37..' });
+  res.render('index', { title: 'Express - Bienes Raices-ISM - MySQL - P38..' });
 });
 
 module.exports = router;
