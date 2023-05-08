0407223d39bb08d4bc10346a4582e6d146c79316
IgorNSMD <insanmartind@gmail.com>
2023-05-08 11:44:25 -0400

    P60a

--- routes/index.js
+++ routes/index.js
@@ -16,8 +16,54 @@ var { Price, Category, Property } = require('../model/index.js')
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express - Bienes Raices-ISM - MySQL - P60..' });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express - Bienes Raices-ISM - MySQL - P60..' });
+// });
+
+//router.get('/', start)
+router.get('/', async (req,res) => {
+    
+  const [ categories, prices, houses, departments ] = await Promise.all([
+      Category.findAll({ raw: true }),
+      Price.findAll({ raw: true }),
+      Property.findAll({ 
+          limit: 3,
+          where: {
+              categoryid: 1
+          },
+          include: [
+              {
+                  model: Price, as:'price'
+              }
+          ],
+          order: [
+              ['createdAt','DESC']
+          ]
+       }),
+       Property.findAll({ 
+          limit: 3,
+          where: {
+              categoryid: 2
+          },
+          include: [
+              {
+                  model: Price, as:'price'
+              }
+          ],
+          order: [
+              ['createdAt','DESC']
+          ]
+       })
+  ])
+
+ // console.log( categories )
+
+  res.render('start',{
+      pageLabel:'Inicio',
+      categories, prices, houses, departments,
+      csrfToken: req.csrfToken(),
+  })
+})
+
 
 module.exports = router;
