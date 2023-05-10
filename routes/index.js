5c3e991ef2c7285aa0862e1d090fb7cadcf81a6d
IgorNSMD <insanmartind@gmail.com>
2023-05-10 14:36:40 -0400

    P64

--- routes/index.js
+++ routes/index.js
@@ -23,43 +23,10 @@ const router = express.Router();
 //router.get('/', start)
 router.get('/', async (req,res) => {
     
-  const [ categories, prices, houses, departments ] = await Promise.all([
-      Category.findAll({ raw: true }),
-      Price.findAll({ raw: true }),
-      Property.findAll({ 
-          limit: 3,
-          where: {
-              categoryid: 1
-          },
-          include: [
-              {
-                  model: Price, as:'price'
-              }
-          ],
-          order: [
-              ['createdAt','DESC']
-          ]
-       }),
-       Property.findAll({ 
-          limit: 3,
-          where: {
-              categoryid: 2
-          },
-          include: [
-              {
-                  model: Price, as:'price'
-              }
-          ],
-          order: [
-              ['createdAt','DESC']
-          ]
-       })
-  ])
-
  // console.log( categories )
 
   res.render('start',{
-      pageLabel:'Inicio P63',
+      pageLabel:'Inicio P64',
       //categories, prices, houses, departments,
       //csrfToken: req.csrfToken(),
   })
